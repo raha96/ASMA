9f4984513bf36d129eb1a2f5f831831a2aa88547
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2023-11-27 23:10:39 -0700

    Add more unit test stubs

--- tests/test_ast_gen.py
+++ tests/test_ast_gen.py
@@ -2,3 +2,65 @@ import pytest
 
 def test_always_passes():
     assert True
+
+def test_delete_multiline_comment():
+    from ast_gen import delete_multiline_comment
+
+    code = "Test code with /* multiline comment */"
+    result = delete_multiline_comment(code)
+    assert result == "Test code with "
+
+def test_report_object():
+    from ast_gen import report_object
+
+    # module = "test_module"
+    # result = report_object(module)
+    assert True
+
+def test_IGNORE():
+    from ast_gen import IGNORE
+
+    # result = IGNORE()
+    assert True
+
+def test_forward_traverse_generic_block():
+    from ast_gen import _forward_traverse_generic_block
+
+    block = "test_block"
+    # result = _forward_traverse_generic_block(block)
+    assert True
+
+def test_forward_traverse_condition():
+    from ast_gen import _forward_traverse_condition
+
+    condition = "test_condition"
+    # result = _forward_traverse_condition(condition)
+    assert True
+
+def test_traverse_block():
+    from ast_gen import traverse_block
+
+    block = "test_block"
+    # result = traverse_block(block)
+    assert True
+
+def test_traverse_lrvalue():
+    from ast_gen import traverse_lrvalue
+
+    value = "test_lrvalue"
+    # result = traverse_lrvalue(value)
+    assert True
+
+def test_traverse_blocking_substitution():
+    from ast_gen import traverse_blocking_substitution
+
+    blocking_substitution = "test_blocking_substitution"
+    # result = traverse_blocking_substitution(blocking_substitution)
+    assert True
+
+def test_traverse_lor():
+    from ast_gen import traverse_lor
+
+    lor = "test_lor"
+    # result = traverse_lor(lor)
+    assert True
